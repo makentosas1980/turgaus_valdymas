020bc4175ff3faab0bc5d81ac5bf647afdf9bbd0
Tomas <sukyst@icloud.com>
2021-05-28 06:19:22 +0300

    Minor changes to property details

--- lib/modules/get_tenant.dart
+++ lib/modules/get_tenant.dart
@@ -60,60 +60,14 @@ class _GetPaymentState extends State<GetPayment> {
         future: paymentt.doc(widget.documentId).get(),
         builder:
             (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text("Kažkas ne taip, bandykite dar kartą vėliau.");
-          }
-
-          if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
             Map<String, dynamic> data = snapshot.data.data();
             var paymentArray = data['payments'];
+            if (snapshot.hasError) {
+              return Text("Kažkas ne taip, bandykite dar kartą vėliau.");
+            }
 
-            //SORT BY DATE
-            paymentArray.sort((a, b) {
-              return a['date'].toString().compareTo(b['date'].toString());
-            });
-
-            paymentArray = paymentArray.reversed.toList();
-            var lastPaymentDate = "${paymentArray[0]['date']}";
-            if (lastPaymentDate == currentDate) {
-              visiblePaymentButton = false;
-              return Container(
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
-                      bottomLeft: Radius.circular(12),
-                      bottomRight: Radius.circular(12)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 4,
-                      blurRadius: 8,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                width: 300,
-                height: 300,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 100.0),
-                      child: Text(
-                        "NUOMOS MOKESTIS SUMOKĖTAS",
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            } else {
+            if (paymentArray.length == 0) {
               return Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
@@ -141,7 +95,6 @@ class _GetPaymentState extends State<GetPayment> {
                               overlayColor:
                                   MaterialStateProperty.all(Colors.blueAccent)),
                           onPressed: () {
-                            print("popas: ${widget.premiseNumberForPayment}");
                             Navigator.push(
                               context,
                               MaterialPageRoute(
@@ -167,7 +120,113 @@ class _GetPaymentState extends State<GetPayment> {
                 ),
               );
             }
+
+            if (snapshot.connectionState == ConnectionState.done &&
+                paymentArray.length != 0) {
+              // Map<String, dynamic> data = snapshot.data.data();
+              // var paymentArray = data['payments'];
+
+              //SORT BY DATE
+              paymentArray.sort((a, b) {
+                return a['date'].toString().compareTo(b['date'].toString());
+              });
+
+              paymentArray = paymentArray.reversed.toList();
+              var lastPaymentDate = "${paymentArray[0]['date']}";
+              if (lastPaymentDate == currentDate) {
+                visiblePaymentButton = false;
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                        bottomLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 4,
+                        blurRadius: 8,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  width: 300,
+                  height: 300,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 100.0),
+                        child: Text(
+                          "NUOMOS MOKESTIS SUMOKĖTAS",
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                  child: Column(
+                    children: [
+                      Text(
+                        "NUOMOS MOKESTIS NESUMOKĖTAS",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.red,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 44,
+                          child: TextButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.blue),
+                                foregroundColor:
+                                    MaterialStateProperty.all(Colors.white),
+                                overlayColor: MaterialStateProperty.all(
+                                    Colors.blueAccent)),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PrintReceipt(
+                                    marketNametoPrint: marketPlaceName,
+                                    pavilionToPrint:
+                                        widget.premiseNumberForPayment,
+                                    premiseNumberToPrint:
+                                        widget.premiseNumberForPayment,
+                                    ammountToPayToPrint:
+                                        '${widget.ammountForPayment}',
+                                    dateToPrint: currentDate,
+                                    documentId: widget.documentId,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Text('Sumokėti'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+            }
           }
+
           return Center(
             child: CircularProgressIndicator(),
           );
